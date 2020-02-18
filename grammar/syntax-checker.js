4bfad5f1e81fd67f386ee7406ec4d0b091742914
coopslarhette <coopslarhette@gmail.com>
2020-02-17 20:46:19 -0800

    fixes linting issues

--- grammar/syntax-checker.js
+++ grammar/syntax-checker.js
@@ -1,6 +1,7 @@
-const fs = require("fs");
-const ohm = require("ohm-js");
+const fs = require('fs')
+const ohm = require('ohm-js')
 
-const grammar = ohm.grammar(fs.readFileSync("grammar/Inkling.ohm"));
+const grammar = ohm.grammar(fs.readFileSync('grammar/Inkling.ohm'))
 
-module.exports = text => grammar.match(text).succeeded();
+module.exports = (text) => grammar.match(text)
+  .succeeded()
