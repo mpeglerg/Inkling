788cab90e133b253c771b5504232edc12dd7c452
coopslarhette <coopslarhette@gmail.com>
2020-02-18 16:59:19 -0800

    adds marco's classes and ast

---
+++ ast/index.js
@@ -0,0 +1,268 @@
+// eslint-disable-next-line max-classes-per-file
+class Program {
+  constructor(stmt) {
+    this.stmt = stmt
+  }
+}
+
+class Assignment {
+  constructor(id, exp) {
+    Object.assign(this, {
+      id,
+      exp,
+    })
+  }
+}
+
+class VarDeclaration {
+  constructor(id, type, exp) {
+    Object.assign(this, {
+      id,
+      type,
+      exp,
+    })
+  }
+}
+
+class Print {
+  constructor(exp) {
+    Object.assign(this, { exp })
+  }
+}
+
+class ReturnStatement {
+  constructor(returnValue) {
+    this.returnValue = returnValue
+  }
+}
+
+class IfStmt {
+  constructor(tests, consequence, alt) {
+    Object.assign(this, {
+      test,
+      consequence,
+      alt,
+    })
+  }
+}
+
+
+class ForStmt {
+  constructor(id, type, exp, body) {
+    Object.assign(this, {
+      index,
+      start,
+      end,
+      body,
+    })
+  }
+}
+
+class FuncDecStmt {
+  constructor(id, params, type, body) {
+    Object.assign(this, {
+      index,
+      start,
+      end,
+      body,
+    })
+  }
+}
+
+class WhileLoop {
+  constructor(condition, body) {
+    Object.assign(this, {
+      condition,
+      body,
+    })
+  }
+}
+
+class FieldVarExp {
+  constructor(id, field) {
+    Object.assign(this, {
+      id,
+      field,
+    })
+  }
+}
+
+class IdentifierExpression {
+  constructor(id) {
+    this.id = id
+  }
+}
+
+class SubscriptedVarExp {
+  constructor(id, key) {
+    Object.assign(this, {
+      id,
+      key,
+    })
+  }
+}
+
+
+class Param {
+  constructor(id, type) {
+    Object.assign(this, {
+      id,
+      type,
+    })
+  }
+}
+
+class Call {
+  constructor(id, args) {
+    Object.assign(this, {
+      id,
+      args,
+    })
+  }
+}
+
+
+class BinaryExpression {
+  constructor(op, left, right) {
+    Object.assign(this, {
+      op,
+      left,
+      right,
+    })
+  }
+}
+
+class PowExp {
+  constructor(left, right) {
+    Object.assign(this, {
+      left,
+      right,
+    })
+  }
+}
+
+class PrefixExpression {
+  constructor(op, operand) {
+    Object.assign(this, {
+      op,
+      operand,
+    })
+  }
+}
+
+class PostfixExpression {
+  constructor(operand, op) {
+    Object.assign(this, {
+      op,
+      operand,
+    })
+  }
+}
+
+
+class Paren {
+  constructor(exp) {
+    Object.assign(this, { exp })
+  }
+}
+
+
+class ListExpression {
+  constructor(members) {
+    this.members = members
+  }
+}
+
+class KeyValueExpression {
+  constructor(key, value) {
+    Object.assign(this, {
+      key,
+      value,
+    })
+  }
+}
+
+class DictExpression {
+  constructor(exp) {
+    Object.assign(this, { exp })
+  }
+}
+
+class SetExpression {
+  constructor(members) {
+    this.members = members
+  }
+}
+
+// Types
+class ListType {
+  constructor(memberType) {
+    Object.assign(this, { memberType })
+  }
+}
+
+class SetType {
+  constructor(memberType) {
+    Object.assign(this, { memberType })
+  }
+}
+
+class DictType {
+  constructor(keyType, valueType) {
+    Object.assign(this, {
+      keyType,
+      valueType,
+    })
+  }
+}
+
+// literals
+class NumericLiteral {
+  constructor(value) {
+    this.value = value
+  }
+}
+
+class TextLiteral {
+  constructor(value) {
+    this.value = value
+  }
+}
+
+class BooleanLiteral {
+  constructor(value) {
+    this.value = value
+  }
+}
+
+// ////// Block
+// Should this be done in parser ? ask
+// class Block {
+//     constructor(statements) {
+//         // map or filter over every statement
+//         this.statements = statements.filter();
+//     }
+// }
+
+module.exports = {
+  Program,
+  Assignment,
+  VarDeclaration,
+  Print,
+  ReturnStatement,
+  IfStmt,
+  ForStmt,
+  FuncDecStmt,
+  WhileLoop,
+  FieldVarExp,
+  SubscriptedVarExp,
+  Param,
+  Call,
+  PlusMinusExp,
+  RelopExp,
+  MulOpExp,
+  AddOpExp,
+  PowExp,
+  PrefixPostfixExp,
+  Paren,
+}
---
+++ ast/parser.js
@@ -0,0 +1,167 @@
+const fs = require('fs')
+const ohm = require('ohm-js')
+const {
+  Program,
+  Assignment,
+  VarDeclaration,
+  Print,
+  Return,
+  IfStmt,
+  ForStmt,
+  FuncDecStmt,
+  WhileStmt,
+  VarExpression,
+  FieldVarExp,
+  SubscriptedVarExp,
+  Param,
+  Call,
+  AndOrExp,
+  AddOpExp,
+  MulOpExp,
+  RelopExp,
+  PowExp,
+  PrefixPostfixExp,
+  Paren,
+} = require('index')
+
+const grammar = ohm.grammar(fs.readFileSync('../grammar/Inkling.ohm'))
+
+function arrayToNullable(a) {
+  return a.length === 0 ? null : a[0]
+}
+
+const astGenerator = grammar.createSemantics()
+  .addOperation('ast', {
+    Program(stmt) {
+      const p = new Program(stmt.ast())
+      return p
+    },
+    SimpleStmt_return(_, e) {
+      return new ReturnStatement(arrayToNullable(e.ast()))
+    },
+    // Statements
+    Block(open, stmts, close) {
+      return new Block(stmts.ast())
+    },
+    IfStmt_ternary(trueTest, _1, test, _2, falseTest) {
+      return new TernaryExpression(test.ast(), trueTest.ast(), falseTest.ast())
+    },
+    IfExpr__if(_1, _2, firstTest, _3, firstBlock, _4, _5, _6, moreTests, _7, moreBlocks, _8, lastBlock) {
+      return new IfStmt([firstTest.ast(), ...moreTests.ast()],
+        [firstBlock.ast(), ...moreBlocks.ast()],
+        arrayToNullable(lastBlock.ast()))
+    },
+    ForLoop(_1, id, _2, type, _3, exp, body) {
+      return new ForStmt(id.ast(), type.ast(), exp.ast(), body.ast())
+    },
+    WhileLoop(_, _open, exp, _close, body) {
+      return new WhileStmt(exp.ast(), body.ast())
+    },
+
+    FuncDec_function(_funcKeyword, id, _open, params, _close, type, body) {
+      return new FuncDecStmt(id.ast(), type.ast(), exp.ast(), body.ast())
+    },
+    FuncDec_ArrowFuncDec(id, _isKeyword, _alwaysKeyword, _open, params, _close, returnType, _arrowSymbol, body) {
+      return new FuncDecStmt(id.ast(), params.ast(), returnType.ast(), body.ast())
+    },
+
+    SimpleStmt_letdec(id, _isKeyword, type, exp) {
+      return new VarDeclaration(id.ast(), type.ast(), exp.ast())
+    },
+    SimpleStmt_constdec(id, _isKeyword, _alwaysKeyword, type, exp) {
+      return new VarDeclaration(id.ast(), type.ast(), exp.ast())
+    },
+
+    Param(id, _isKeyword, type) {
+      return new Param(id.ast(), type.ast())
+    },
+    Call(callName, _1, args, _2) {
+      return new Call(callName.ast(), args.ast())
+    },
+    id(_1, _2) {
+      return this.sourceString
+    },
+
+    KeyValue(id, _, exp) {
+      return new KeyValueExpression(id.ast(), exp.ast())
+    },
+
+    SimpleStmt_print(_displayKeyword, exp) {
+      return SimpleStmt_Print(exp.ast())
+    },
+
+    // Expressions
+    Exp_or(left, op, right) {
+      return new BinaryExpression(op.ast(), left.ast(), right.ast())
+    },
+    Exp_and(left, op, right) {
+      return new BinaryExpression(op.ast(), left.ast(), right.ast())
+    },
+    Exp1_binary(left, op, right) {
+      return new BinaryExpression(op.ast(), left.ast(), right.ast())
+    },
+    Exp2_binary(left, op, right) {
+      return new BinaryExpression(op.ast(), left.ast(), right.ast())
+    },
+    Exp3_binary(left, op, right) {
+      return new BinaryExpression(op.ast(), left.ast(), right.ast())
+    },
+    Exp5_prefix(op, operand) {
+      return new PrefixExpression(op.ast(), operand.ast())
+    },
+    Exp4_binary(left, op, right) {
+      return new BinaryExpression(op.ast(), left.ast(), right.ast())
+    },
+    Exp5_postfix(operand, op) {
+      return new PostfixExpression(operand.ast(), op.ast())
+    },
+    Exp5_parens(_1, expression, _2) {
+      return expression.ast()
+    },
+    Exp5_list(_1, expressions, _2) {
+      return new ListExpression(expressions.ast())
+    },
+    Exp5_set(_1, expressions, _2) {
+      return new SetExpression(expressions.ast())
+    },
+    Exp5_dict(_1, keyValue, _2) {
+      return new DictionaryExpression(keyValue.ast())
+    },
+    VarExp_field(id, _dotOperator, field) {
+      return new FieldVarExp(id.ast(), field.ast())
+    },
+    VarExp_subscripted(id, _open, key, _close) {
+      return new SubscriptedVarExp(id.ast(), key.ast())
+    },
+    VarExp_simple(id) {
+      return new IdentifierExpression(id.ast())
+    },
+
+    // Types
+    ListType(_1, type, _2) {
+      return new ListType(type.ast())
+    },
+    SetType(_1, type, _2) {
+      return new SetType(type.ast())
+    },
+    DictType(_1, keyType, _2, valueType, _3) {
+      return new DictType(keyType.ast(), valueType.ast())
+    },
+
+    // literals
+    numlit(_1, _2, _3, _4, _5, _6) {
+      return new NumericLiteral(+this.sourceString)
+    },
+    txtlit(_1, chars, _2) {
+      return new StringLiteral(this.sourceString)
+    },
+    boollit(_) {
+      return new BooleanLiteral(this.sourceString === 'true')
+    },
+    // SimpleStmt_return(_gimmeKeyword, exp) {
+    //     if (!exp.ast()) {
+    //         return new Return();
+    //     }
+    //     return new Return(exp.ast());
+    // }
+  })
