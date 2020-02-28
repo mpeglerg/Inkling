4e1b18f198f2bf72922b9049761b7e52aafc4e08
mpeglerg <mpeglerg@lion.lmu.edu>
2020-02-27 17:28:25 -0800

    added tests

--- ast/__tests__/parser.test.js
+++ ast/__tests__/parser.test.js
@@ -11,94 +11,117 @@
  * Based on toal's iki parser.test.js
  */
 
-const parse = require('../parser')
+const parse = require("../parser");
 
 const {
   Program,
   Block,
-  // Assignment,
-  // we have assignment parsing functions but the classes don't match
-  // I think we don't need Assignment class
+  Assignment,
   VarDeclaration,
   Print,
   ReturnStatement,
-  ForLoop,
   IfStmt,
+  ForLoop,
   FuncDecStmt,
   WhileLoop,
   FieldVarExp,
+  IdentifierExpression,
   SubscriptedVarExp,
   Param,
   Call,
   BinaryExpression,
-  IdentifierExpression,
+  PowExp,
+  PrefixExpression,
+  PostfixExpression,
   ListExpression,
   KeyValueExpression,
   DictExpression,
   SetExpression,
-  PowExp,
-  PrefixExpression,
-  PostfixExpression,
-  // Paren,  // not needed I think
   ListType,
   SetType,
   DictType,
   NumericLiteral,
   TextLiteral,
-  BooleanLiteral,
-} = require('../index')
+  BooleanLiteral
+} = require("../index");
 
 const fixture = {
   declarations: [
     String.raw`y is Text "Hello World!"
     `,
-    new Program(
-      [
-        new VarDeclaration('y', false, 'Text',
-          new TextLiteral('"Hello World!"'))],
-    ),
+    new Program([
+      new VarDeclaration("y", false, "Text", new TextLiteral('"Hello World!"'))
+    ]),
     String.raw`x is always Num 5
     `,
-    new Program(
-      new Block(
-        [
-          new NumericLiteral('5'),
-          new VarDeclaration('x', true, 'Num')],
-      ),
-    ),
+    new Program([
+      new NumericLiteral("5"),
+      new VarDeclaration("x", true, "Num")
+    ]),
     String.raw`x is Bool true
     `, // false?
-    new Program(
-      new Block(
-        [
-          new BooleanLiteral('true'),
-          new VarDeclaration('x', true, 'Bool')],
-      ),
-    ),
+    new Program([
+      new BooleanLiteral("true"),
+      new VarDeclaration("x", true, "Bool")
+    ]),
     String.raw`x is always Num 5
     `,
-    new Program(
-      new Block(
-        [
-          new NumericLiteral(5),
-          new VarDeclaration('x', true, 'Num')],
-      ),
-    ),
+    new Program([new NumericLiteral(5), new VarDeclaration("x", true, "Num")])
   ],
 
-  // whiles: [
-  //   String.raw`while false loop x = 3; end;`,
-  //   new Program(
-  //     new Block([
-  //       new WhileStatement(
-  //         new BooleanLiteral(false),
-  //         new Block([
-  //           new AssignmentStatement(new VariableExpression('x'),
-  //             new IntegerLiteral('3'))]),
-  //       ),
-  //     ]),
-  //   ),
-  // ],
+  functions: [
+    String.raw`
+    function f(x is Num, y is Num) is Num {
+      gimme x + y
+    }
+    `,
+    new Program(
+      new FuncDecStmt(
+        "f",
+        new Param("x", "Num"),
+        new Param("y", "Num"),
+        "Num",
+        new Block([new ReturnStatement(new BinaryExpression("+", "x", "y"))])
+      )
+    )
+  ],
+
+  whiles: [
+    String.raw`
+      x is Num 2
+      y is Num 4
+      while(x <= y) {
+        display 'hi'
+        x++
+      }
+  `,
+    new Program([
+      new WhileLoop(
+        new BinaryExpression(
+          "<=",
+          new VarDeclaration("x", false, "Num", new NumericLiteral(2)),
+          new VarDeclaration("y", false, "Num", new NumericLiteral(4))
+        ),
+        new Block([new Print(new TextLiteral("hi"))]),
+        new PostfixExpression("x", "++")
+      )
+    ])
+  ]
+
+  // fors: [
+  //   String.raw`
+  //   for x is Num in range(0, 10){
+  //     display 'hi'
+  //   }
+  // `,
+  //   new Program([
+  //     new ForLoop(
+  //       new IdentifierExpression('x'),
+  //       new
+  //     )
+  //   ])
+  // ]
+
   //
   // math: [
   //   String.raw`read x, y; write 2 * (-5 > 7+1);`,
@@ -140,20 +163,20 @@ const fixture = {
   //     ]),
   //   ),
   // ],
-}
+};
 
-describe('The parser', () => {
+describe("The parser", () => {
   Object.entries(fixture).forEach(([name, [source, expected]]) => {
-    test(`produces the correct AST for ${name}`, (done) => {
-      expect(parse(source)).toEqual(expected)
-      done()
-    })
-  })
+    test(`produces the correct AST for ${name}`, done => {
+      expect(parse(source)).toEqual(expected);
+      done();
+    });
+  });
 
-  test('throws an exception on a syntax error', (done) => {
+  test("throws an exception on a syntax error", done => {
     // We only need one test here that an exception is thrown.
     // Specific syntax errors are tested in the grammar test.
-    expect(() => parse('as$df^&%*$&')).toThrow()
-    done()
-  })
-})
+    expect(() => parse("as$df^&%*$&")).toThrow();
+    done();
+  });
+});
