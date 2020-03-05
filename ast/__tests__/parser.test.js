220e9db6771858fc86c82614022df50fdd4dcbae
mpeglerg <mpeglerg@lion.lmu.edu>
2020-03-04 21:10:15 -0800

    added list, set, dict tests. Working on while

--- ast/__tests__/parser.test.js
+++ ast/__tests__/parser.test.js
@@ -11,82 +11,122 @@
  * Based on toal's iki parser.test.js
  */
 
-const parse = require('../parser')
+const parse = require("../parser");
 
 const {
-  Program,
-  Block,
+  Program, //done
+  Block, //done
   Assignment,
-  VarDeclaration,
-  Print,
-  ReturnStatement,
-  IfStmt,
+  VarDeclaration, //done
+  Print, //done
+  ReturnStatement, //done
+  IfStmt, //done -- need to test ternaries
   ForLoop,
-  FuncDecStmt,
-  WhileLoop,
+  FuncDecStmt, //done
+  WhileLoop, // Maya and Talia
   FieldVarExp,
-  IdentifierExpression,
+  IdentifierExpression, //done
   SubscriptedVarExp,
-  Param,
+  Param, //done
   Call,
-  BinaryExpression,
+  BinaryExpression, //done
   PowExp,
   PrefixExpression,
   PostfixExpression,
-  ListExpression,
-  KeyValueExpression,
-  DictExpression,
-  SetExpression,
-  ListType,
-  SetType,
-  DictType,
-  NumericLiteral,
-  TextLiteral,
-  BooleanLiteral,
-} = require('../index')
+  ListExpression, //done
+  KeyValueExpression, //done
+  DictExpression, //done
+  SetExpression, //done
+  ListType, //done
+  SetType, //done
+  DictType, //done
+  NumericLiteral, //done
+  TextLiteral, //done
+  BooleanLiteral //done
+} = require("../index");
 
 const fixture = {
   declarations: [
     String.raw`y is Text "Hello World!"
     `,
-    new Program(
-      [
-        new VarDeclaration('y', false, 'Text',
-          new TextLiteral('Hello World!'))],
-    ),
+    new Program([
+      new VarDeclaration("y", false, "Text", new TextLiteral("Hello World!"))
+    ]),
     String.raw`x is always Num 5
     `,
     new Program(
-      new Block(
-        [
-          new NumericLiteral(5),
-          new VarDeclaration('x', true, 'Num')],
-      ),
+      new Block([new NumericLiteral(5), new VarDeclaration("x", true, "Num")])
     ),
     String.raw`x is Bool true
     `,
-    new Program(
-      [
-        new BooleanLiteral('true'),
-        new VarDeclaration('x', false, 'Bool')],
-    ),
+    new Program([
+      new BooleanLiteral("true"),
+      new VarDeclaration("x", false, "Bool")
+    ]),
     String.raw`x is Bool false
     `,
-    new Program(
-      [
-        new BooleanLiteral('false'),
-        new VarDeclaration('x', false, 'Bool')],
-    ),
+    new Program([
+      new BooleanLiteral("false"),
+      new VarDeclaration("x", false, "Bool")
+    ]),
+    String.raw` ageDictionary is Dict<Text, Num> {"Sam": 21, "Talia":20}
+    `,
+    [
+      new Program([
+        new VarDeclaration(
+          "ageDictionary",
+          false,
+          new DictType("Text", "Num"),
+          new DictExpression([
+            new KeyValueExpression(
+              new TextLiteral("Sam"),
+              new NumericLiteral(21)
+            ),
+            new KeyValueExpression(
+              new TextLiteral("Talia"),
+              new NumericLiteral(20)
+            )
+          ])
+        )
+      ])
+    ]
   ],
 
   printing: [
     String.raw`display 5
     `,
-    new Program(
-      [
-        new Print(new NumericLiteral(5)),
-      ],
-    ),
+    new Program([new Print(new NumericLiteral(5))])
+  ],
+
+  set: [
+    String.raw`aSetOfNums is Set<Num> {1, 2}
+  `,
+
+    new Program([
+      new VarDeclaration(
+        "aSetOfNums",
+        false,
+        new SetType("Num"),
+        new SetExpression([new NumericLiteral(1), new NumericLiteral(2)])
+      )
+    ])
+  ],
+
+  list: [
+    String.raw`ourList is List<Text> ["this", "a", "list"]
+    `,
+    new Program([
+      new VarDeclaration(
+        "ourList",
+        false,
+        new ListType("Text"),
+        new ListExpression([
+          new TextLiteral("this"),
+          new TextLiteral("a"),
+          new TextLiteral("list")
+        ])
+      )
+    ])
   ],
 
   functions: [
@@ -95,47 +135,47 @@ const fixture = {
       gimme x + y
     }
     `,
-    new Program(
-      [
-        new FuncDecStmt(
-          'f',
-          [
-            new Param('x', 'Num'),
-            new Param('y', 'Num'),
-          ],
-          'Num',
-          new Block([
-            new ReturnStatement(
-              new BinaryExpression('+', new IdentifierExpression('x'),
-                new IdentifierExpression('y')),
-            ),
-          ]),
-        ),
-      ],
-    ),
+    new Program([
+      new FuncDecStmt(
+        "f",
+        [new Param("x", "Num"), new Param("y", "Num")],
+        "Num",
+        new Block([
+          new ReturnStatement(
+            new BinaryExpression(
+              "+",
+              new IdentifierExpression("x"),
+              new IdentifierExpression("y")
+            )
+          )
+        ])
+      )
+    ]),
     String.raw`
     x is always (x is Num, y is Num) is Num => {
       gimme x + y
     }
     `,
-    new Program(
-      [
-        new VarDeclaration('x', true, new FuncDecStmt( // i feel like there should be a return node here but tests pass..
-          'f',
-          [
-            new Param('x', 'Num'),
-            new Param('y', 'Num'),
-          ],
-          'Num',
+    new Program([
+      new VarDeclaration(
+        "x",
+        true,
+        new FuncDecStmt( // i feel like there should be a return node here but tests pass..
+          "f",
+          [new Param("x", "Num"), new Param("y", "Num")],
+          "Num",
           new Block([
             new ReturnStatement(
-              new BinaryExpression('+', new IdentifierExpression('x'),
-                new IdentifierExpression('y')),
-            ),
-          ]),
-        )),
-      ],
-    ),
+              new BinaryExpression(
+                "+",
+                new IdentifierExpression("x"),
+                new IdentifierExpression("y")
+              )
+            )
+          ])
+        )
+      )
+    ]),
     String.raw`
     function helloWorld() is Void {
       display "Hello world!
@@ -144,52 +184,103 @@ const fixture = {
     `,
     new Program(
       new FuncDecStmt(
-        'hellowWorld',
+        "hellowWorld",
         [],
-        'Void',
-        new Block([
-          new Print(
-            new TextLiteral('Hello world!'),
-          ),
-        ]),
+        "Void",
+        new Block([new Print(new TextLiteral("Hello world!"))])
+      )
+    )
+  ],
+
+  // whiles: [
+  //   String.raw`while false loop x = 3; end;`,
+  //   new Program(
+  //     new Block([
+  //       new WhileStatement(
+  //         new BooleanLiteral(false),
+  //         new Block([
+  //           new AssignmentStatement(new VariableExpression('x'),
+  //             new IntegerLiteral('3'))]),
+  //       ),
+  //     ]),
+  //   ),
+  // ],
+
+  while: [
+    String.raw`
+      i is Num 10
+      while(i > 0) {
+        --i
+    }
+    `,
+    new Program([
+      new VarDeclaration(
+        new TextLiteral("i"),
+        false,
+        "Num",
+        new NumericLiteral(10)
       ),
-    ),
+      new WhileLoop(
+        new BinaryExpression(
+          ">",
+          new IdentifierExpression(new TextLiteral("i"), new NumericLiteral(10))
+        ),
+        new Block(new PrefixExpression("--", new IdentifierExpression("i")))
+      )
+    ])
   ],
 
   math: [
     String.raw`
       result is Num 3 + 10 / 5 - 3 % 2
     `,
-    new Program(
-      [
-        new VarDeclaration('result', false, 'Num',
-          new BinaryExpression('-',
-            new BinaryExpression('+', new NumericLiteral(3),
-              new BinaryExpression('/', new NumericLiteral(10),
-                new NumericLiteral(5))),
-            new BinaryExpression('%', new NumericLiteral(3),
-              new NumericLiteral(2)))),
-      ],
-    ),
+    new Program([
+      new VarDeclaration(
+        "result",
+        false,
+        "Num",
+        new BinaryExpression(
+          "-",
+          new BinaryExpression(
+            "+",
+            new NumericLiteral(3),
+            new BinaryExpression(
+              "/",
+              new NumericLiteral(10),
+              new NumericLiteral(5)
+            )
+          ),
+          new BinaryExpression(
+            "%",
+            new NumericLiteral(3),
+            new NumericLiteral(2)
+          )
+        )
+      )
+    ]),
     String.raw`
       result is Num 2^3
     `,
-    new Program(
-      [
-        new VarDeclaration('result', false, 'Num', new PowExp(2, 3)),
-      ],
-    ),
+    new Program([new VarDeclaration("result", false, "Num", new PowExp(2, 3))]),
     String.raw`
       result is Num 3 * (3 + 2)
     `,
-    new Program(
-      [
-        new VarDeclaration('result', false, 'Num',
-          new BinaryExpression('*', new NumericLiteral(3),
-            new BinaryExpression('+', new NumericLiteral(2),
-              new NumericLiteral(2)))),
-      ],
-    ),
+    new Program([
+      new VarDeclaration(
+        "result",
+        false,
+        "Num",
+        new BinaryExpression(
+          "*",
+          new NumericLiteral(3),
+          new BinaryExpression(
+            "+",
+            new NumericLiteral(2),
+            new NumericLiteral(2)
+          )
+        )
+      )
+    ])
   ],
 
   ifElses: [
@@ -204,32 +295,67 @@ const fixture = {
     }
     `,
     [
-      new VarDeclaration('x', false, 'Num', 6),
-      new IfStmt(new BinaryExpression('<', 'x', new NumericLiteral(10)), true, false),
-    ],
+      new VarDeclaration("x", false, "Num", 6),
+      new IfStmt(
+        new BinaryExpression("<", "x", new NumericLiteral(10)),
+        true,
+        false
+      )
+    ]
   ],
   idExpressions: [
     String.raw`field is Bool
     `,
-    [
-      new IdentifierExpression('field'),
-    ],
-  ],
+    [new IdentifierExpression("field")]
+  ]
+};
 
-}
-
-describe('The parser', () => {
+describe("The parser", () => {
   Object.entries(fixture).forEach(([name, [source, expected]]) => {
-    test(`produces the correct AST for ${name}`, (done) => {
-      expect(parse(source)).toEqual(expected)
-      done()
-    })
-  })
+    test(`produces the correct AST for ${name}`, done => {
+      expect(parse(source)).toEqual(expected);
+      done();
+    });
+  });
 
-  test('throws an exception on a syntax error', (done) => {
+  test("throws an exception on a syntax error", done => {
     // We only need one test here that an exception is thrown.
     // Specific syntax errors are tested in the grammar test.
-    expect(() => parse('as$df^&%*$&')).toThrow()
-    done()
-  })
-})
+    expect(() => parse("as$df^&%*$&")).toThrow();
+    done();
+  });
+});
+//       display x
+//     } else {
+//       display -1
+//     }
+//     `,
+//     [
+//       new VarDeclaration('x', false, 'Num', 6),
+//       new IfStmt(new BinaryExpression('<', 'x', new NumericLiteral(10)), true, false),
+//     ],
+//   ],
+
+idExpressions: [
+  String.raw`field is Bool
+    `,
+  [new IdentifierExpression("field")]
+];
+
+// }
+
+// describe('The parser', () => {
+//   Object.entries(fixture).forEach(([name, [source, expected]]) => {
+//     test(`produces the correct AST for ${name}`, (done) => {
+//       expect(parse(source)).toEqual(expected)
+//       done()
+//     })
+//   })
+
+//   test('throws an exception on a syntax error', (done) => {
+//     // We only need one test here that an exception is thrown.
+//     // Specific syntax errors are tested in the grammar test.
+//     expect(() => parse('as$df^&%*$&')).toThrow()
+//     done()
+//   })
+// })
