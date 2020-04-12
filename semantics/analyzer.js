e5fdac7153f98022daebeb9787fefafdc365a1c0
coopslarhette <coopslarhette@gmail.com>
2020-04-12 14:59:06 -0700

    adds block analyzer

--- semantics/analyzer.js
+++ semantics/analyzer.js
@@ -1,5 +1,6 @@
 const {
   Program,
+  Block,
   Assignment,
   VarDeclaration,
   Literal,
@@ -21,229 +22,232 @@ const {
   PostfixExpression,
   PrefixExpression,
   ForLoop,
-} = require("../ast/index");
+} = require('../ast/index')
 
-const check = require("../semantics/check");
+const check = require('../semantics/check')
 
 const {
   NumType,
   BoolType,
   TextType,
   NoneType,
-} = require("../semantics/builtins");
+} = require('../semantics/builtins')
 
-const Context = require("./context");
+const Context = require('./context')
 
 module.exports = function (root) {
-  root.analyze(Context.INITIAL);
-};
+  root.analyze(Context.INITIAL)
+}
 
 Program.prototype.analyze = function (context) {
-  console.log("These are statements : ", this);
+  console.log('These are statements : ', this)
   this.stmts.forEach((stmt) => {
-    stmt.analyze(context);
-  });
-};
+    stmt.analyze(context)
+  })
+}
+
+Block.prototype.analyze = function (context) {
+  const localContext = context.createChildContextForBlock()
+  this.statements.forEach((s) => s.analyze(localContext))
+}
 
 // design decisions need to be made for this
 VarDeclaration.prototype.analyze = function (context) {
-  this.exp.analyze(context);
-  this.type = context.lookupValue(this.type);
-  check.isAssignableTo(this.exp, this.type);
-  const a = new Assignment(this.id, this.exp);
-  a.analyze(context);
-};
+  this.exp.analyze(context)
+  this.type = context.lookupValue(this.type)
+  check.isAssignableTo(this.exp, this.type)
+  // const a = new Assignment(this.id, this.exp)
+  context.add(this, this.id)
+}
 
 Assignment.prototype.analyze = function (context) {
-  console.log("Assigment ID: ", this.id, "Exp: ", this.exp);
-
-  // this.id.analyze(context);
-  // this.exp.analyze(context);
-  //check.isAssignableTo(this.id, this.exp.type);
-  //check.isNotReadOnly(this.exp);
-};
+  console.log('Assigment ID: ', this.id, 'Exp: ', this.exp)
+  context.lookupValue(this.id)
+  check.isAssignableTo(this.id, this.exp.type)
+  check.isNotReadOnly(this.exp)
+}
 
 Literal.prototype.analyze = function (context) {
-  if (typeof this.value === "number") {
-    this.type = NumType;
-  } else if (typeof this.value === "boolean") {
-    this.type = BoolType;
-  } else if (typeof this.value === "string") {
-    this.type = TextType;
+  if (typeof this.value === 'number') {
+    this.type = NumType
+  } else if (typeof this.value === 'boolean') {
+    this.type = BoolType
+  } else if (typeof this.value === 'string') {
+    this.type = TextType
   } else {
-    this.type = NoneType;
+    this.type = NoneType
   }
-};
+}
 
 IfStmt.prototype.analyze = function (context) {
   this.tests.forEach((test) => {
-    test.analyze(context);
-    check.isBool(test); // Add boolean checker to check file
-  });
+    test.analyze(context)
+    check.isBool(test) // Add boolean checker to check file
+  })
   this.consequence.forEach((block) => {
-    const blockContext = context.createChildContextForBlock();
-    block.forEach((statement) => statement.analyze(blockContext));
-  });
+    const blockContext = context.createChildContextForBlock()
+    block.forEach((statement) => statement.analyze(blockContext))
+  })
   if (this.alt) {
-    const alternateBlock = context.createChildContextForBlock();
-    this.alt.forEach((s) => s.analyze(alternateBlock));
+    const alternateBlock = context.createChildContextForBlock()
+    this.alt.forEach((s) => s.analyze(alternateBlock))
   }
-};
+}
 
 BinaryExpression.prototype.analyze = function (context) {
-  this.left.analyze(context);
-  this.right.analyze(context);
-  if (["<=", ">=", "<", ">"].includes(this.op)) {
-    check.isNum(this.left);
-    check.isNum(this.right);
-    this.type = BoolType;
-  } else if (["!=", "=="].includes(this.op)) {
-    check.expressionsHaveTheSameType(this.left.type, this.right.type);
-    this.type = BoolType;
-  } else if (["and", "or"].includes(this.op)) {
-    check.isBool(this.left);
-    check.isBool(this.right);
-    this.type = BoolType;
-  } else if (this.op === "+") {
-    check.expressionsHaveTheSameType(this.left.type, this.right.type);
-    check.isNumOrText(this.left);
-    check.isNumOrText(this.right);
-    this.type = this.left.type === NumType ? NumType : BoolType;
+  this.left.analyze(context)
+  this.right.analyze(context)
+  if (['<=', '>=', '<', '>'].includes(this.op)) {
+    check.isNum(this.left)
+    check.isNum(this.right)
+    this.type = BoolType
+  } else if (['!=', '=='].includes(this.op)) {
+    check.expressionsHaveTheSameType(this.left.type, this.right.type)
+    this.type = BoolType
+  } else if (['and', 'or'].includes(this.op)) {
+    check.isBool(this.left)
+    check.isBool(this.right)
+    this.type = BoolType
+  } else if (this.op === '+') {
+    check.expressionsHaveTheSameType(this.left.type, this.right.type)
+    check.isNumOrText(this.left)
+    check.isNumOrText(this.right)
+    this.type = this.left.type === NumType ? NumType : BoolType
   } else {
-    check.expressionsHaveTheSameType(this.left.type, this.right.type);
-    this.type = NumType;
+    check.expressionsHaveTheSameType(this.left.type, this.right.type)
+    this.type = NumType
   }
-};
+}
 
 PrefixExpression.prototype.analyze = function (context) {
-  if ("!" == this.op) {
-    check.isBool(this.operand);
-    this.type = BoolType;
+  if ('!' == this.op) {
+    check.isBool(this.operand)
+    this.type = BoolType
   } else {
-    check.isNum(this.operand);
-    this.type = NumType;
+    check.isNum(this.operand)
+    this.type = NumType
   }
-};
+}
 
 PostfixExpression.prototype.analyze = function (context) {
-  check.isNum(this.operand);
-  this.type = NumType;
-};
+  check.isNum(this.operand)
+  this.type = NumType
+}
 
 WhileLoop.prototype.analyze = function (context) {
-  this.condition.analyze(context);
-  const bodyContext = context.createChildContextForLoop();
-  this.body.forEach((s) => s.analyze(bodyContext));
-};
+  this.condition.analyze(context)
+  const bodyContext = context.createChildContextForLoop()
+  this.body.forEach((s) => s.analyze(bodyContext))
+}
 
 ForLoop.prototype.analyze = function (context) {
   // TODO
-  this.exp.analyze(context);
+  this.exp.analyze(context)
   // check list, obj, or set
-};
+}
 
 FuncDecStmt.prototype.analyze = function (context) {
-  context.add(this.function.id, this);
-  const bodyContext = context.createChildContextForFunctionBody(this);
-  this.body.forEach((s) => s.analyze(bodyContext));
-};
+  context.add(this.function.id, this)
+  const bodyContext = context.createChildContextForFunctionBody(this)
+  this.body.forEach((s) => s.analyze(bodyContext))
+}
 
 FuncObject.prototype.analyze = function (context) {
-  this.params = this.params.map((p) => new Param(p.type, p.id));
-  this.params.forEach((p) => p.analyze(context));
-  this.body.forEach((s) => s.analyze(context));
+  this.params = this.params.map((p) => new Param(p.type, p.id))
+  this.params.forEach((p) => p.analyze(context))
+  this.body.forEach((s) => s.analyze(context))
 
   const returnStatement = this.body.filter(
-    (b) => b.constructor === ReturnStatement
-  );
-  if (returnStatement.length === 0 && this.type !== "void") {
-    throw new Error("No return statement found");
+    (b) => b.constructor === ReturnStatement,
+  )
+  if (returnStatement.length === 0 && this.type !== 'void') {
+    throw new Error('No return statement found')
   } else if (returnStatement.length > 0) {
-    if (this.type === "void") {
-      throw new Error("Void functions do not have return statements");
+    if (this.type === 'void') {
+      throw new Error('Void functions do not have return statements')
     }
-    check.isAssignableTo(returnStatement[0].returnValue.type, this.type);
+    check.isAssignableTo(returnStatement[0].returnValue.type, this.type)
   }
-};
+}
 
 Param.prototype.analyze = function (context) {
-  context.add(this.id, this);
-};
+  context.add(this.id, this)
+}
 
 ReturnStatement.prototype.analyze = function (context) {
-  this.returnValue.analyze(context);
-  context.assertInFunction("Return statement not in function");
-};
+  this.returnValue.analyze(context)
+  context.assertInFunction('Return statement not in function')
+}
 
 DictExpression.prototype.analyze = function (context) {
   this.exp.forEach((e) => {
-    e.key.analyze(context);
-    e.value.analyze(context);
-  });
+    e.key.analyze(context)
+    e.value.analyze(context)
+  })
   if (this.exp.length) {
-    const keyType = this.exp[0].key.type;
-    const valueType = this.exp[0].value.type;
-    this.type = new DictType(keyType, valueType);
+    const keyType = this.exp[0].key.type
+    const valueType = this.exp[0].value.type
+    this.type = new DictType(keyType, valueType)
     for (let i = 1; i < this.exp.length; i += 1) {
-      check.expressionsHaveTheSameType(this.exp[i].key.type, this.type.keyType);
+      check.expressionsHaveTheSameType(this.exp[i].key.type, this.type.keyType)
       check.expressionsHaveTheSameType(
         this.exp[i].value.type,
-        this.type.valueType
-      );
+        this.type.valueType,
+      )
     }
   }
-};
+}
 
 ListExpression.prototype.analyze = function (context) {
-  this.members.forEach((m) => m.analyze(context));
+  this.members.forEach((m) => m.analyze(context))
   if (this.members.length) {
-    this.type = new ListType(this.members[0].type);
+    this.type = new ListType(this.members[0].type)
     for (let i = 1; i < this.members.length; i += 1) {
       check.expressionsHaveTheSameType(
         this.members[i].type,
-        this.type.memberType
-      );
+        this.type.memberType,
+      )
     }
   }
-};
+}
 
 SetExpression.prototype.analyze = function (context) {
-  this.members.forEach((m) => m.analyze(context));
+  this.members.forEach((m) => m.analyze(context))
   if (this.members.length) {
-    this.type = new SetType(this.members[0].type);
+    this.type = new SetType(this.members[0].type)
     for (let i = 1; i < this.members.length; i += 1) {
       check.expressionsHaveTheSameType(
         this.members[i].type,
-        this.type.memberType
-      );
+        this.type.memberType,
+      )
     }
   }
-};
+}
 
 Call.prototype.analyze = function (context) {
-  this.id.analyze(context);
-  this.args.forEach((arg) => arg.analyze(context));
-  this.type = this.id.ref.type;
-  context.isFunction(this.id.ref);
+  this.id.analyze(context)
+  this.args.forEach((arg) => arg.analyze(context))
+  this.type = this.id.ref.type
+  context.isFunction(this.id.ref)
   if (this.args.length !== this.id.ref.params.length) {
-    throw new Error("Incorrect number of arguments");
+    throw new Error('Incorrect number of arguments')
   }
   this.args.forEach((a, i) => {
-    const paramType = this.id.ref.params[i].type;
+    const paramType = this.id.ref.params[i].type
     if (check.isListType(paramType)) {
       if (
         a.expression.type.constructor !== paramType.constructor &&
-        paramType !== "void"
+        paramType !== 'void'
       ) {
-        throw new Error("Argument and Param types do not match");
+        throw new Error('Argument and Param types do not match')
       }
-    } else if (a.expression.type !== paramType && paramType !== "void") {
-      throw new Error("Argument and Param types do not match");
+    } else if (a.expression.type !== paramType && paramType !== 'void') {
+      throw new Error('Argument and Param types do not match')
     }
-  });
-};
+  })
+}
 
 IdentifierExpression.prototype.analyze = function (context) {
-  this.ref = context.lookupValue(this.id);
-  this.type = this.ref.type;
-};
+  this.ref = context.lookupValue(this.id)
+  this.type = this.ref.type
+}
