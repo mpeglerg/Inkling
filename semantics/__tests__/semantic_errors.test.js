24704f489e705015daeeaab01ff2965536dbe802
Sam gibson <samgibson@sams-mbp.attlocal.net>
2020-04-13 20:25:51 -0700

    fixed variable names in error tests

--- semantics/__tests__/semantic_errors.test.js
+++ semantics/__tests__/semantic_errors.test.js
@@ -9,10 +9,10 @@ const parse = require("../../ast/parser");
 const Context = require("../context");
 
 const errors = [
-  ["use of undeclared variable", "x is 1\n"],
-  ["assignment to constant", "x is always Num 5\nx is 1\n"],
+  ["use of undeclared variable", "a is 1\n"],
+  ["assignment to constant", "b is always Num 5\nx is 1\n"],
   ["List with inconsistent types", 'c is List<Text> ["this", 2, "b"]\n'],
-  ["Set with inconsistent types", 'c is Set<Text> {"this", 2, "b"}\n'],
+  ["Set with inconsistent types", 'd is Set<Text> {"this", 2, "b"}\n'],
   [
     "Dict with inconsistent types",
     'ageDictionary is Dict<Text, Text> {"Sam": 21, "Talia": 20}\n',
@@ -28,36 +28,36 @@ const errors = [
   ["non integer in subtract", '"dog" - 5\n'],
   ["types do not match in equality test", '2 == "dog"\n'],
   ["types do not match in inequality test", '2 < "dog"\n'],
-  ["types do not match in declaration", 'x isNum "hello"\n'],
+  ["types do not match in declaration", 'e isNum "hello"\n'],
   [
     "undeclared because in other scope",
-    "x is Num 5\n function fun1 (h is Num, i is Num) is Void {\ndisplay x\ngimme 4\n}\n",
+    "f is Num 5\n function fun2 (h is Num, i is Num) is Void {\ndisplay x\ngimme 4\n}\n",
   ],
   [
     "undeclared because in opposite scope",
-    "function fun1 (h is Num, i is Num) is Void {\nx is Num 5\ngimme 4\n}\n display x \n",
+    "function fun3 (h is Num, i is Num) is Void {\nx is Num 5\ngimme 4\n}\n display x \n",
   ],
-  ["redeclaration of variable", "x isNum 3\nx isNum 4\n"],
-  ["type mismatch in assignment", 'x isNum 3\nx is "hello"\n'],
-  ["writing to (readonly) for loop index", "x is always Num 5\n x is 3\n"],
+  ["redeclaration of variable", "g isNum 3\ng isNum 4\n"],
+  ["type mismatch in assignment", 'h isNum 3\nx is "hello"\n'],
+  ["writing to (readonly) for loop index", "i is always Num 5\n i is 3\n"],
   ["too many function arguments", "abs(1, 2, 3)\n"],
   ["too few function arguments", "pow(5)\n"],
   ["wrong type of function argument", 'abs("hi")\n'],
   // ["redeclared field", 'd is Dict<Text, Num> {"r": 0, "r": 3}\n'], // just overwrite the field, can change if have time
-  ["no such field", 'p is Dict<Text, Num> {"Sam": 21}\n p["hi"]\n'],
+  ["no such field", 'j is Dict<Text, Num> {"Sam": 21}\n p["hi"]\n'],
   //   ['member of nonrecord', 'let var x := 3 in x.y end'],
-  ["subscript of nonarray", "x is Num 5\nx[0]\n"],
-  ["call of nonfunction", "x is Num 5 \n x(5) \n"],
-  ["non integer subscript", 'arr is List<Text> [1,2,3]\n arr["lol"]\n'],
+  ["subscript of nonarray", "k is Num 5\nx[0]\n"],
+  ["call of nonfunction", "l is Num 5 \n x(5) \n"],
+  ["non integer subscript", 'm is List<Text> [1,2,3]\n m["lol"]\n'],
   [
     "no return statement in function",
-    "function f (x is Num) is Num {display x + 2\n}\n",
+    "function fun4 (x is Num) is Num {display x + 2\n}\n",
   ],
   [
     "function returns the wrong type",
-    "function f (x is Num) is Text {gimme x + 2\n}\n",
+    "function fun5 (x is Num) is Text {gimme x + 2\n}\n",
   ],
-  ["return statement outside of a function", "a is Num 5\n"],
+  ["return statement outside of a function", "n is Num 5\n gimme n\n"],
   //   // Might need more here, depending on your test coverage report
 ];
 
