1062d0bbc691a74658cfa39211d94fbf97241dec
coopslarhette <coopslarhette@gmail.com>
2020-04-14 22:51:58 -0700

    context cleaned up and linted

--- semantics/context.js
+++ semantics/context.js
@@ -14,9 +14,9 @@ const {
   TextType,
   BoolType,
   NoneType,
-} = require("./builtins");
+} = require('./builtins')
 
-require("./analyzer");
+require('./analyzer')
 
 // When doing semantic analysis we pass around context objects.
 //
@@ -41,7 +41,7 @@ class Context {
       inLoop,
       declarations: Object.create(null),
       typeMap: Object.create(null),
-    });
+    })
   }
 
   createChildContextForFunctionBody(currentFunction) {
@@ -49,16 +49,15 @@ class Context {
       parent: this,
       currentFunction,
       inLoop: false,
-    });
+    })
   }
 
   createChildContextForLoop() {
-    //console.log("in the contxt for loop", this);
     return new Context({
       parent: this,
       currentFunction: this.currentFunction,
       inLoop: true,
-    });
+    })
   }
 
   createChildContextForBlock() {
@@ -66,58 +65,28 @@ class Context {
       parent: this,
       currentFunction: this.currentFunction,
       inLoop: this.inLoop,
-    });
+    })
   }
 
   add(id, entity) {
-    // console.log("id ", id, " entity ", entity);
-    //console.log("All declarations", this.declarations);
     if (id in this.declarations) {
-      // console.log("this is the id: ", id);
-      throw new Error(`${id} already declared in this scope`);
+      throw new Error(`${id} already declared in this scope`)
     }
-    this.declarations[id] = entity;
+    this.declarations[id] = entity
   }
 
   lookupValue(id) {
-    //console.log("this is the id :", id);
     for (let context = this; context !== null; context = context.parent) {
-      //console.log("context look up id: ", context.declarations);
       if (id in context.declarations) {
-        //console.log("context look up id: ", context.declarations[id]);
-        return context.declarations[id];
+        return context.declarations[id]
       }
     }
-    throw new Error(`Identifier ${id} has not been declared`);
+    throw new Error(`Identifier ${id} has not been declared`)
   }
-
-  // variableMustNotBeAlreadyDeclared(id) {
-  //   if (this.declarations[id]) {
-  //     throw new Error(`Variable ${id} already declared`);
-  //   }
-  // }
-  // hasBeenDeclared(id) {
-  //   if (this.declarations[id]) {
-  //     return true;
-  //   } else if (this.parent !== null) {
-  //     return this.parent.hasBeenDeclared(id);
-  //   }
-  //   return false;
-  // }
-
-  // checkIfVariableIsAlreadyDeclared(id) {
-  //   if (this.declarations[id]) {
-  //     throw new Error(`Variable ${id} already declared`);
-  //   }
-  //   if (this.parent !== null) {
-  //     return this.parent.checkIfVariableIsAlreadyDeclared(id);
-  //   }
-  //   return 0;
-  // }
 
   assertInFunction(message) {
     if (!this.currentFunction) {
-      throw new Error(message);
+      throw new Error(message)
     }
   }
 }
@@ -133,7 +102,7 @@ Context.INITIAL = new Context();
   ...mathFunctions,
   ...textFunctions,
 ].forEach((entity) => {
-  Context.INITIAL.add(entity.id, entity);
-});
+  Context.INITIAL.add(entity.id, entity)
+})
 
-module.exports = Context;
+module.exports = Context
