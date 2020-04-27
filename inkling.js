d430faa3fefcffebf27584608f25f7a5cc812546
coopslarhette <coopslarhette@gmail.com>
2020-04-26 20:30:59 -0700

    updates node terminal stuff

--- inkling.js
+++ inkling.js
@@ -7,36 +7,57 @@ const fs = require('fs')
 const util = require('util')
 const yargs = require('yargs')
 const parse = require('./ast/parser')
+const Context = require('./semantics/context')
+const generate = require('./backend/javascript-generator')
 
-// Build and return ast
-function compile(sourceCode) {
+// If compiling from a string, return the AST, IR, or compiled code as a string.
+function compile(sourceCode, { astOnly, frontEndOnly, shouldOptimize }) {
   const program = parse(sourceCode)
-  return util.inspect(program, { depth: null })
+  if (astOnly) {
+    return util.inspect(program, { depth: null })
+  }
+  program.analyze(Context.INITIAL)
+  // if (shouldOptimize) {
+  //   program = program.optimize()
+  // }
+  if (frontEndOnly) {
+    return util.inspect(program, { depth: null })
+  }
+  return generate(program)
 }
 
 // If compiling from a file, write to standard output.
 function compileFile(filename, options) {
   fs.readFile(filename, 'utf-8', (error, sourceCode) => {
     if (error) {
-      // eslint-disable-next-line no-console
       console.error(error)
       return
     }
 
-    // eslint-disable-next-line no-console
     console.log(compile(sourceCode, options))
   })
+}
+
+module.exports = {
+  compile,
+  compileFile,
 }
 
 // If running as a script, we have a lot of command line processing to do. The source
 // program will come from the file name that is given as the command line argument.
 if (require.main === module) {
-  const { argv } = yargs
-    .usage('$0 [-a] filename')
-    .boolean(['a'])
-    .describe('a', 'show abstract syntax tree after parsing then stop')
-    .demand(1)
+  const { argv } = yargs.usage('$0 [-a] [-o] [-i] filename').
+    boolean(['a', 'o', 'i']).
+    describe('a', 'show abstract syntax tree after parsing then stop').
+    describe('o', 'do optimizations').
+    describe(
+      'i',
+      'generate and show the decorated abstract syntax tree then stop',
+    ).
+    demand(1)
   compileFile(argv._[0], {
     astOnly: argv.a,
+    frontEndOnly: argv.i,
+    // shouldOptimize: argv.o,
   })
 }
