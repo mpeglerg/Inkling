a62a50770fb2da009591a533fc98a9577afcf7a8
Veda Ashok <vashok061@gmail.com>
2020-04-28 16:26:39 -0700

    Put in BinaryExpression optimizer from Tiger

--- semantics/optimizer.js
+++ semantics/optimizer.js
@@ -26,3 +26,32 @@ const {
 } = require('../ast');
 
 module.exports = program => program.optimize();
+
+function isZero(e) {
+  return e instanceof Literal && e.value === 0;
+}
+
+function isOne(e) {
+  return e instanceof Literal && e.value === 1;
+}
+
+function bothLiterals(b) {
+  return b.left instanceof Literal && b.right instanceof Literal;
+}
+BinaryExpression.prototype.optimize = function () {
+  this.left = this.left.optimize();
+  this.right = this.right.optimize();
+  if (this.op === '+' && isZero(this.right)) return this.left;
+  if (this.op === '+' && isZero(this.left)) return this.right;
+  if (this.op === '*' && isZero(this.right)) return new Literal(0);
+  if (this.op === '*' && isZero(this.left)) return new Literal(0);
+  if (this.op === '*' && isOne(this.right)) return this.left;
+  if (this.op === '*' && isOne(this.left)) return this.right;
+  if (bothLiterals(this)) {
+    const [x, y] = [this.left.value, this.right.value];
+    if (this.op === '+') return new Literal(x + y);
+    if (this.op === '*') return new Literal(x * y);
+    if (this.op === '/') return new Literal(x / y);
+  }
+  return this;
+};
